5cbda8cdb2e43dda20c9cb2cfaf143f1e3a81bb5
Teodor <teodortenchev@gmail.com>
2019-03-04 00:25:31 +0400

    Create VillainNames.cs

---
+++ Problem2/VillainNames.cs
@@ -0,0 +1,39 @@
+﻿using AdoNet_Exercises;
+using System;
+using System.Data.SqlClient;
+
+namespace Problem2
+{
+    public class VillainNames
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection connection = new SqlConnection(Configuration.ConnectionString))
+            {
+                connection.Open();
+
+                string sqlQuery = @"  SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount  
+                                            FROM Villains AS v 
+                                            JOIN MinionsVillains AS mv ON v.Id = mv.VillainId 
+                                        GROUP BY v.Id, v.Name 
+                                          HAVING COUNT(mv.VillainId) > 3 
+                                        ORDER BY COUNT(mv.VillainId)";
+
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            //Can also reference colums with numbers e.g. reader[0] is the same as reader["Name"]
+                            string name = (string)reader["Name"];
+                            int minionsCount = (int)reader["MinionsCount"];
+
+                            Console.WriteLine($"{name} - {minionsCount}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

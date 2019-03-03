3ea745518da3f32d09be24f08edf35a91e029de4
Teodor <teodortenchev@gmail.com>
2019-03-04 00:25:24 +0400

    Create Configuration.cs

---
+++ Problem1_DB_Setup/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace AdoNet_Exercises
+{
+    public static class Configuration
+    {
+        public const string ConnectionString = @"Server=localhost\SQLEXPRESS;Database=MinionsDB;Integrated Security=true";
+    }
+}

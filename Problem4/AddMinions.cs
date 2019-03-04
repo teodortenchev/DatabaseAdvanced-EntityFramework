bdac916438564c7dbeaa15bc3773b55917fecfb4
Teodor <teodortenchev@gmail.com>
2019-03-04 13:42:45 +0400

    extracting methods

--- Problem4/AddMinions.cs
+++ Problem4/AddMinions.cs
@@ -20,26 +20,51 @@ namespace Problem4
                 string minionTown = minionInfo[3];
                 string villainName = villain[1];
 
-                string checkTownExists = "SELECT Id FROM Towns WHERE Name = @Name";
 
-                using (SqlCommand command = new SqlCommand(checkTownExists, connection))
+                CheckTownInDB(connection, minionTown);
+
+
+
+            }
+        }
+
+        private static void CheckTownInDB(SqlConnection connection, string minionTown)
+        {
+            string checkTownExists = "SELECT Id FROM Towns WHERE Name = @Name";
+
+            using (SqlCommand command = new SqlCommand(checkTownExists, connection))
+            {
+                command.Parameters.AddWithValue("@Name", minionTown);
+
+                int? id = (int?)command.ExecuteScalar();
+
+                if (id == null)
                 {
-                    command.Parameters.AddWithValue("@Name", minionTown);
-
-                    int? id = (int?)command.ExecuteScalar();
-
-                    if(id == null)
-                    {
-                        AddTown(connection, minionTown);
-                    }
+                    AddTown(connection, minionTown);
                 }
-
             }
         }
 
         private static void AddTown(SqlConnection connection, string townName)
         {
-            throw new NotImplementedException();
+            string insertTown = "INSERT INTO Towns (Name) VALUES (@townName)";
+
+            using (SqlCommand command = new SqlCommand(insertTown, connection))
+            {
+                command.Parameters.AddWithValue("@townName", townName);
+
+                int rowsAffected = (int)command.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    Console.WriteLine($"Town {townName} was added to the database.");
+                }
+                else
+                {
+                    throw new InvalidOperationException($"Something went wrong while attempting to add {townName} to the Database");
+                }
+
+            }
         }
     }
 }

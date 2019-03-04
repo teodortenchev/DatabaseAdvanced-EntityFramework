60dcba22d47d1a1e9d90cdc6923735a63475dd9b
Teodor <teodortenchev@gmail.com>
2019-03-04 23:23:37 +0400

    Create PrintMinionNames.cs

---
+++ Prolem7/PrintMinionNames.cs
@@ -0,0 +1,51 @@
+﻿using AdoNet_Exercises;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Prolem7
+{
+    class PrintMinionNames
+    {
+        private static List<string> minionNames;
+
+        static void Main(string[] args)
+        {
+            minionNames = new List<string>();
+
+            GetMinionNamesFromDB();
+
+            for (int i = 0; i < minionNames.Count / 2; i++)
+            {
+                Console.WriteLine(minionNames[i]);
+                Console.WriteLine(minionNames[minionNames.Count - 1 - i]);
+            }
+
+            if (minionNames.Count % 2 != 0)
+            {
+                Console.WriteLine(minionNames[minionNames.Count / 2]);
+            }
+        }
+
+        private static void GetMinionNamesFromDB()
+        {
+            using (SqlConnection connection = new SqlConnection(Configuration.ConnectionString))
+            {
+                connection.Open();
+
+                string getMinionNamesSql = @"SELECT [Name] FROM Minions";
+
+                using (SqlCommand command = new SqlCommand(getMinionNamesSql, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            minionNames.Add((string)reader["Name"]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

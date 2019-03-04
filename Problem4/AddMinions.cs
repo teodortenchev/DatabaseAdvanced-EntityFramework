1f2d699922257ee682d2bb4af3488ceba15e8c3b
Teodor <teodortenchev@gmail.com>
2019-03-04 13:33:49 +0400

    Create AddMinions.cs

---
+++ Problem4/AddMinions.cs
@@ -0,0 +1,45 @@
+﻿using AdoNet_Exercises;
+using System;
+using System.Data.SqlClient;
+
+namespace Problem4
+{
+    class AddMinions
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection connection = new SqlConnection(Configuration.ConnectionString))
+            {
+                connection.Open();
+
+                string[] minionInfo = Console.ReadLine().Split();
+                string[] villain = Console.ReadLine().Split();
+
+                string minionName = minionInfo[1];
+                int minionAge = int.Parse(minionInfo[2]);
+                string minionTown = minionInfo[3];
+                string villainName = villain[1];
+
+                string checkTownExists = "SELECT Id FROM Towns WHERE Name = @Name";
+
+                using (SqlCommand command = new SqlCommand(checkTownExists, connection))
+                {
+                    command.Parameters.AddWithValue("@Name", minionTown);
+
+                    int? id = (int?)command.ExecuteScalar();
+
+                    if(id == null)
+                    {
+                        AddTown(connection, minionTown);
+                    }
+                }
+
+            }
+        }
+
+        private static void AddTown(SqlConnection connection, string townName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

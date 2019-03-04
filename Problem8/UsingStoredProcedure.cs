e11358125b4dacc30e068d4c79a63849cb510aa9
Teodor <teodortenchev@gmail.com>
2019-03-04 23:59:23 +0400

    update

---
+++ Problem8/UsingStoredProcedure.cs
@@ -0,0 +1,37 @@
+﻿using AdoNet_Exercises;
+using System;
+using System.Data.SqlClient;
+
+namespace Problem8
+{
+    class UsingStoredProcedure
+    {
+        static void Main(string[] args)
+        {
+            int minionId = int.Parse(Console.ReadLine());
+
+            using (SqlConnection connection = new SqlConnection(Configuration.ConnectionString))
+            {
+                connection.Open();
+
+                string uspGetOlderProc = "EXEC usp_GetOlder @id";
+
+                using (SqlCommand command = new SqlCommand(uspGetOlderProc, connection))
+                {
+                    command.Parameters.AddWithValue("@id", minionId);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        reader.Read();
+                        string name = (string)reader[0];
+                        int age = (int)reader[1];
+
+                        Console.WriteLine($"{name} - {age}");
+                    }
+                }
+
+                
+            }
+        }
+    }
+}

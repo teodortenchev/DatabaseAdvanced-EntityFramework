a3e3dd84b6abde05238172c7aaedac49e08f335e
Teodor <teodortenchev@gmail.com>
2019-03-06 22:17:56 +0400

    Implemented Change Tracker

---
+++ 02_MiniOrm_LiveCoding/MiniORM/DbContext.cs
@@ -0,0 +1,19 @@
+﻿namespace MiniORM
+{
+    using System;
+
+    public abstract class DbContext
+    {
+        internal static readonly Type[] AllowedSqlTypes =
+        {
+            typeof(string),
+            typeof(int),
+            typeof(uint),
+            typeof(long),
+            typeof(ulong),
+            typeof(decimal),
+            typeof(bool),
+            typeof(DateTime),
+        };
+    }
+}
---
+++ 02_MiniOrm_LiveCoding/MiniORM/DbSet.cs
@@ -0,0 +1,8 @@
+﻿namespace MiniORM
+{
+    using System.Collections.Generic;
+    public class DbSet<T>
+    {
+        public List<T> Entities { get; set; }
+    }
+}

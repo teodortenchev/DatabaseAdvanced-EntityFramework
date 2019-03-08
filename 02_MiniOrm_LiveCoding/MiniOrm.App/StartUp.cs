58fc265be5e83e94c1562325ef5a2abc877e6bdc
Teodor <teodortenchev@gmail.com>
2019-03-08 20:46:17 +0400

    .

--- 02_MiniOrm_LiveCoding/MiniOrm.App/StartUp.cs
+++ 02_MiniOrm_LiveCoding/MiniOrm.App/StartUp.cs
@@ -7,9 +7,22 @@
     {
         static void Main(string[] args)
         {
-            var connectionString = "Sever=.;Database=MiniORM;Integrated Security=True";
+            var connectionString = @"Server=localhost\SQLEXPRESS;Database=MinionsDB;Integrated Security=true";
 
             var context = new SoftUniDbContext(connectionString);
+
+            context.Employees.Add(new Employee
+            {
+                FirstName = "Gosho",
+                LastName = "Inserted",
+                DepartmentId = context.Departments.First().Id,
+                IsEmployed = true,
+            });
+
+            var employee = context.Employees.Last();
+            employee.FirstName = "Modified";
+
+            context.SaveChanges();
         }
     }
 }

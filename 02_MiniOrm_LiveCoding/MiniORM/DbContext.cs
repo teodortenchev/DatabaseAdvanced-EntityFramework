6498991726a8e996672627f02a67def6e1d49918
Teodor <teodortenchev@gmail.com>
2019-03-08 14:31:03 +0400

    Update DbContext.cs

--- 02_MiniOrm_LiveCoding/MiniORM/DbContext.cs
+++ 02_MiniOrm_LiveCoding/MiniORM/DbContext.cs
@@ -1,9 +1,57 @@
 ﻿namespace MiniORM
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
 
     public abstract class DbContext
     {
+        private readonly DatabaseConnection connection;
+        private readonly Dictionary<Type, PropertyInfo> dbSetProperties;
+
+        protected DbContext(string connectionString)
+        {
+            this.connection = new DatabaseConnection(connectionString);
+
+            this.dbSetProperties = this.DiscoverDbSets();
+
+            using (new ConnectionManager(connection))
+            {
+                this.InitializeDbSets();
+            }
+
+
+            this.MapAllRelations();
+        }
+
+        public void SaveChanges()
+        {
+            var dbSets = this.dbSetProperties
+                .Select(pi => pi.Value.GetValue(this))
+                .ToArray();
+
+            foreach (IEnumerable<object> dbSet in dbSets)
+            {
+                var validateEntities = dbSet.
+            }
+        }
+
+        private void MapAllRelations()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void InitializeDbSets()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Dictionary<Type, PropertyInfo> DiscoverDbSets()
+        {
+            throw new NotImplementedException();
+        }
+
         internal static readonly Type[] AllowedSqlTypes =
         {
             typeof(string),
@@ -15,5 +63,7 @@
             typeof(bool),
             typeof(DateTime),
         };
+
+
     }
 }

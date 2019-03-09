36a3d666a5cb4ce725e05680b625ea44a4aa2961
Teodor <teodortenchev@gmail.com>
2019-03-09 20:07:58 +0400

    Problem 7 Solution

---
+++ 03_EntityFramework_Intro_Exercises/07_EmployeesAndProjects/StartUp.cs
@@ -0,0 +1,60 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SoftUniContext())
+            {
+                string result = GetEmployeesInPeriod(context);
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string GetEmployeesInPeriod(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employeesInPeriod = context.Employees
+                .Where(p => p.EmployeesProjects.Any(start => start.Project.StartDate.Year >= 2001
+                        && start.Project.StartDate.Year <= 2003))
+                .Select(e => new
+                {
+                    EmployeeFullName = e.FirstName + ' ' + e.LastName,
+                    ManagerFullName = e.Manager.FirstName + ' ' + e.Manager.LastName,
+                    Projects = e.EmployeesProjects.Select(p => new
+                    {
+                        ProjectName = p.Project.Name,
+                        StartDate = p.Project.StartDate,
+                        EndDate = p.Project.EndDate
+                    })
+                })
+                .Take(10)
+                .ToList();
+
+            foreach (var employee in employeesInPeriod)
+            {
+                var projects = employee.Projects;
+
+                sb.AppendLine($"{employee.EmployeeFullName} - Manager: {employee.ManagerFullName}");
+
+                foreach (var project in projects)
+                {
+                    string format = "M/d/yyyy h:mm:ss tt";
+                    string startDate = project.StartDate.ToString(format, CultureInfo.InvariantCulture);
+                    string endDate = project.EndDate != null ? project.EndDate.Value.ToString(format, CultureInfo.InvariantCulture) : "not finished";
+
+                    sb.AppendLine($"--{project.ProjectName} - {startDate} - {endDate}");
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}

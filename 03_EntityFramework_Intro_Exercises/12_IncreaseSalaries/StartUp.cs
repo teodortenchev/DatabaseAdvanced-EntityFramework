d6e425c5a8373c5d3696a1763a89d0f7142bf3ed
Teodor <teodortenchev@gmail.com>
2019-03-10 00:07:54 +0400

    Restoring, accidental overwriting

--- 03_EntityFramework_Intro_Exercises/12_IncreaseSalaries/StartUp.cs
+++ 03_EntityFramework_Intro_Exercises/12_IncreaseSalaries/StartUp.cs
@@ -6,51 +6,37 @@
     using System.Linq;
     using System.Text;
 
+
     public class StartUp
     {
         static void Main(string[] args)
         {
             using (var context = new SoftUniContext())
             {
-                string result = AddNewAddressToEmployee(context);
+                string result = IncreaseSalaries(context);
                 Console.WriteLine(result);
             }
         }
 
-        public static string AddNewAddressToEmployee(SoftUniContext context)
+        public static string IncreaseSalaries(SoftUniContext context)
         {
             StringBuilder sb = new StringBuilder();
 
-            var address = new Address()
+            var employees = context.Employees
+                .Where(e => e.Department.Name == "Engineering" || e.Department.Name == "Tool Design"
+                        || e.Department.Name == "Marketing" || e.Department.Name == "Information Services")
+                .OrderBy(e => e.FirstName).ThenBy(e => e.LastName).ToList();
+
+            foreach (var employee in employees)
             {
-                AddressText = "Vitoshka 15",
-                TownId = 4
-
-            };
-
-            //Entity Framework can add the below automatically if it does not exist, so not needed
-            context.Addresses.Add(address);
-
-            var nakov = context.Employees
-                .FirstOrDefault(x => x.LastName == "Nakov");
-
-            nakov.Address = address;
-
-            context.SaveChanges();
-        
-            var employeeAddresses = context.Employees
-                .OrderByDescending(x => x.AddressId)
-                .Select(x => x.Address.AddressText)
-                .Take(10)
-                .ToList();
-
-            foreach (var addr in employeeAddresses)
-            {
-                sb.AppendLine(addr);
+                employee.Salary *= 1.12M;
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} (${employee.Salary:F2})");
             }
 
+            context.SaveChanges();
+
             return sb.ToString().TrimEnd();
-            
+
         }
     }
 }

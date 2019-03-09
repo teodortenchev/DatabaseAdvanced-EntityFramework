7eea659e218ae51329edb38c18d8da3ba31d701e
Teodor <teodortenchev@gmail.com>
2019-03-09 16:45:31 +0400

    new

---
+++ 03_EntityFramework_Intro_Exercises/04_EmployeesSalaryOver50000/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SoftUniContext())
+            {
+                string result = GetEmployeesWithSalaryOver50000(context);
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+                            .Select(e => new
+                            {
+                                e.FirstName,
+                                e.Salary
+                            })
+                            .Where(e => e.Salary > 50000)
+                            .OrderBy(e => e.FirstName)
+                            .ToList();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} - {employee.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}

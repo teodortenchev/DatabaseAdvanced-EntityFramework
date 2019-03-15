817b09337f502d2c057e5f16b89b87b71aeaa702
Teodor <teodortenchev@gmail.com>
2019-03-15 21:04:59 +0400

    models

---
+++ P03_FootballBetting/Data/Config.cs
@@ -0,0 +1,8 @@
+﻿namespace P03_FootballBetting.Data
+{
+    public class Config
+    {
+        public const string ConnectionString = "Server=DESKTOP-NM32SG1\\SQLEXPRESS;Database=FootballBettingDB;Integrated Security=True;";
+
+    }
+}
---
+++ P03_FootballBetting/Data/FootballBettingContext .cs
@@ -0,0 +1,35 @@
+﻿namespace P03_FootballBetting.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using P03_FootballBetting.Data.Models;
+
+    public class FootballBettingContext  : DbContext
+    {
+       
+        public FootballBettingContext(DbContextOptions options)
+              : base(options)
+        {
+
+        }
+
+        //DbSets
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Config.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //ConfigureResources(modelBuilder);
+            
+            
+           
+        }
+
+       
+    }
+}

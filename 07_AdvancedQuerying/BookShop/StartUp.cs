aa848fa734065af239e8fc4eaaaad4335bd3d2ec
Teodor <teodortenchev@gmail.com>
2019-03-24 23:18:15 +0400

    Update StartUp.cs

--- 07_AdvancedQuerying/BookShop/StartUp.cs
+++ 07_AdvancedQuerying/BookShop/StartUp.cs
@@ -18,7 +18,9 @@
             using (var db = new BookShopContext())
             {
                 // DbInitializer.ResetDatabase(db);
-                Console.WriteLine(GetBooksByAuthor(db, "po"));
+                int result = RemoveBooks(db);
+
+                Console.WriteLine(result);
             }
         }
 
@@ -165,7 +167,7 @@
             string nameStart = input.ToLower();
 
             var books = context.Books
-                .Where(b => b.Author.LastName.ToLower().StartsWith(nameStart))
+                .Where(b => EF.Functions.Like(b.Author.LastName, $"{input}%"))
                 .OrderBy(b => b.BookId)
                 .Select(b => new { Title = b.Title, AuthorName = b.Author.FirstName + " " + b.Author.LastName })
                 .ToList();
@@ -173,6 +175,129 @@
             string result = String.Join(Environment.NewLine, books.Select(b => $"{b.Title} ({b.AuthorName})"));
 
             return result;
+        }
+
+        /// <summary>
+        /// Task 10. Return the number of books, which have a title longer than the number given as an input.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="lengthCheck"></param>
+        /// <returns></returns>
+        public static int CountBooks(BookShopContext context, int lengthCheck)
+        {
+            int countBooks = context.Books.Where(b => b.Title.Length > lengthCheck).Count();
+
+            return countBooks;
+        }
+
+        /// <summary>
+        /// Task 11. Return the total number of book copies for each author. Order the results descending by total book copies.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public static string CountCopiesByAuthor(BookShopContext context)
+        {
+            var bookCopiesByAuthor = context.Authors.Select(x => new
+            {
+                Count = x.Books.Select(b => b.Copies).Sum(),
+                AuthorName = x.FirstName + " " + x.LastName
+            })
+            .OrderByDescending(x => x.Count)
+            .ToList();
+
+            string result = String.Join(Environment.NewLine, bookCopiesByAuthor.Select(x => $"{x.AuthorName} - {x.Count}"));
+
+            return result;
+        }
+
+        /// <summary>
+        /// Task 12. Return the total profit of all books by category. Profit for a book can be calculated by multiplying its number of copies 
+        /// bythe price per single book. Order the results by descending by total profit for category and ascending by category name.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public static string GetTotalProfitByCategory(BookShopContext context)
+        {
+            var revenueByCategory = context.Categories.Select(x => new
+            {
+                CategoryName = x.Name,
+                Revenue = x.CategoryBooks.Select(cb => cb.Book.Copies * cb.Book.Price).Sum()
+            })
+            .OrderByDescending(x => x.Revenue).ThenBy(x => x.CategoryName)
+
+            .ToList();
+
+            string result = String.Join(Environment.NewLine, revenueByCategory.Select(x => $"{x.CategoryName} ${x.Revenue}"));
+
+            return result;
+        }
+
+        /// <summary>
+        /// Task 13. Gets the top 3 most recent books by category. Ordered by release date.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public static string GetMostRecentBooks(BookShopContext context)
+        {
+            var mostRecentByCategory = context.Categories.Include(x => x.CategoryBooks).ThenInclude(x => x.Book).ToList()
+                .Select(x => new
+                {
+                    CategoryName = x.Name,
+
+                    Top3RecentBooks = x.CategoryBooks
+                    .Select(b => b.Book)
+                    .OrderByDescending(b => b.ReleaseDate.Value)
+                    .Take(3)
+                })
+                .OrderBy(x => x.CategoryName)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var category in mostRecentByCategory)
+            {
+                sb.AppendLine($"--{category.CategoryName}");
+
+                foreach (var book in category.Top3RecentBooks)
+                {
+                    sb.AppendLine($"{book.Title} ({book.ReleaseDate.Value.Year})");
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        /// <summary>
+        /// Task 14. Increases the prices of all books released before 2010 by 5. 
+        /// </summary>
+        /// <param name="context"></param>
+        public static void IncreasePrices(BookShopContext context)
+        {
+            var booksBefore2005 = context.Books.Where(b => b.ReleaseDate.Value.Year < 2010).ToList();
+
+            foreach (var book in booksBefore2005)
+            {
+                book.Price += 5;
+            }
+
+            context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Task 15. Removes all books which have less than 4200 copies. 
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns>Returns the number of deleted books as int.</returns>
+        public static int RemoveBooks(BookShopContext context)
+        {
+            var booksToDelete = context.Books.Where(x => x.Copies < 4200);
+
+
+            context.Books.RemoveRange(booksToDelete);
+
+            int affectedRows = context.SaveChanges();
+
+            return affectedRows;
         }
     }
 }

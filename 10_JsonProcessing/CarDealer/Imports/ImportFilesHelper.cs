be5227b4ab5263ea4da1fb247b01768f22b2118b
Teodor Tenchev <teodortenchev@gmail.com>
2019-04-04 00:52:08 +0400

    Using relative file paths.

--- 10_JsonProcessing/CarDealer/Imports/ImportFilesHelper.cs
+++ 10_JsonProcessing/CarDealer/Imports/ImportFilesHelper.cs
@@ -2,11 +2,11 @@
 {
     public static class ImportFilesHelper
     {
-        public static string CarsJsonPath => @"F:\C# Fundamentals\DB Advanced\Car Dealer - Skeleton\CarDealer\Datasets\cars.json";
-        public static string PartsJsonPath => @"F:\C# Fundamentals\DB Advanced\Car Dealer - Skeleton\CarDealer\Datasets\parts.json";
-        public static string SalesJsonPath => @"F:\C# Fundamentals\DB Advanced\Car Dealer - Skeleton\CarDealer\Datasets\sales.json";
-        public static string SuppliersJsonPath => @"F:\C# Fundamentals\DB Advanced\Car Dealer - Skeleton\CarDealer\Datasets\suppliers.json";
-        public static string CustomersJsonPath => @"F:\C# Fundamentals\DB Advanced\Car Dealer - Skeleton\CarDealer\Datasets\customers.json";
+        public static string CarsJsonPath => @"..\..\..\Datasets\cars.json";
+        public static string PartsJsonPath => @"..\..\..\Datasets\parts.json";
+        public static string SalesJsonPath => @"..\..\..\Datasets\sales.json";
+        public static string SuppliersJsonPath => @"..\..\..\Datasets\suppliers.json";
+        public static string CustomersJsonPath => @"..\..\..\Datasets\customers.json";
 
     }
 }

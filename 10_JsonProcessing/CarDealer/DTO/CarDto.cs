3db0bc0223d7bead194f35533628605bad764a27
Teodor Tenchev <teodortenchev@gmail.com>
2019-04-04 00:51:51 +0400

    Create CarDto.cs

---
+++ 10_JsonProcessing/CarDealer/DTO/CarDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace CarDealer.DTO
+{
+    public class CarDto
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public long TravelledDistance { get; set; }
+
+        public List<int> PartsId { get; set; }
+
+
+    }
+}

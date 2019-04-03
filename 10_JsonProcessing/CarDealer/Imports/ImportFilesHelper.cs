8a8f0f7f5c88db92b46120b47c39fe8182f354f5
Teodor Tenchev <teodortenchev@gmail.com>
2019-04-03 11:40:40 +0400

    setting up

---
+++ 10_JsonProcessing/CarDealer/Imports/ImportFilesHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace CarDealer.Imports
+{
+    public static class ImportFilesHelper
+    {
+        public static string CarsJsonPath => @"F:\C# Fundamentals\DB Advanced\Car Dealer - Skeleton\CarDealer\Datasets\cars.json";
+        public static string PartsJsonPath => @"F:\C# Fundamentals\DB Advanced\Car Dealer - Skeleton\CarDealer\Datasets\parts.json";
+        public static string SalesJsonPath => @"F:\C# Fundamentals\DB Advanced\Car Dealer - Skeleton\CarDealer\Datasets\sales.json";
+        public static string SuppliersJsonPath => @"F:\C# Fundamentals\DB Advanced\Car Dealer - Skeleton\CarDealer\Datasets\suppliers.json";
+        public static string CustomersJsonPath => @"F:\C# Fundamentals\DB Advanced\Car Dealer - Skeleton\CarDealer\Datasets\customers.json";
+
+    }
+}
---
+++ 10_JsonProcessing/CarDealer/StartUp.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AutoMapper;
+using CarDealer.Data;
+using CarDealer.Imports;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        
+        public static void Main(string[] args)
+        {
+            using(CarDealerContext context = new CarDealerContext())
+            {
+                context.Database.EnsureCreated();
+            }
+        }
+
+
+
+        public static void DBInitializeFromJson(CarDealerContext context)
+        {
+            ImportSuppliers(context, ImportFilesHelper.SuppliersJsonPath);
+            ImportParts(context, ImportFilesHelper.PartsJsonPath);
+            ImportCars(context, ImportFilesHelper.CarsJsonPath);
+            ImportCustomers(context, ImportFilesHelper.CustomersJsonPath);
+            ImportSales(context, ImportFilesHelper.SalesJsonPath);
+
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            return $"Successfully imported {recordsCount}.";
+        }
+
+        public static string ImportParts(CarDealerContext context, string partsJsonPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ImportCars(CarDealerContext context, string partsJsonPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ImportCustomers(CarDealerContext context, string partsJsonPath)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public static string ImportSales(CarDealerContext context, string partsJsonPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        
+
+
+        
+
+        
+    }
+}

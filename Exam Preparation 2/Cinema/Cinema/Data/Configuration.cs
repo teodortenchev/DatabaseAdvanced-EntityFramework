4a6263a0fa5394de833d8cb8df91b48e7cd43298
Teodor Tenchev <teodortenchev@gmail.com>
2019-04-12 11:55:15 +0400

    started new trial exam
    13 mins taken to build db from scratch

---
+++ Exam Preparation 2/Cinema/Cinema/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Cinema.Data
+{
+   public static class Configuration
+    {
+        public static string ConnectionString = @"Server=Server=DESKTOP-5BS58RU\SQLEXPRESS;Database=Cinema;Trusted_Connection=True";
+    }
+}
